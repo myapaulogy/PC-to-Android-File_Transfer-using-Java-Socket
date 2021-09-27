0d4f44146168a5b53b685792fd8d2c9601b7fd48
Paul Antonescu <paul.antscu@gmail.com>
2021-09-27 12:40:16 -0400

    Add IP search
    Displays all possible IP address in the gui

--- For PC/GUI application/IPServer GUI (source code)/src/PcGui.java
+++ For PC/GUI application/IPServer GUI (source code)/src/PcGui.java
@@ -1,19 +1,55 @@
 import javax.swing.*;
-import java.awt.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 public class PcGui extends IPServer{
     public static void main(String[] args) {
 
+        // Get Possible IP addresses
+        ArrayList<String> ip = new ArrayList<>();
+        try {
+            Enumeration<NetworkInterface> Interfaces = NetworkInterface.getNetworkInterfaces();
+            while(Interfaces.hasMoreElements())
+            {
+                NetworkInterface Interface = Interfaces.nextElement();
+                Enumeration<InetAddress> Addresses = Interface.getInetAddresses();
+                while(Addresses.hasMoreElements())
+                {
+                    InetAddress Address = Addresses.nextElement();
+                    System.out.println(Address.getHostAddress());
+
+                    ip.add(Address.getHostAddress());
+                }
+            }
+
+        } catch (SocketException e) {
+
+            e.printStackTrace();
+
+        }
+        JList<String> ipList = new JList<>(ip.toArray(new String[0]));
+
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder());
-        panel.setLayout(new GridLayout());
+        panel.setLayout(null);
+
+        JLabel ipAddress = new JLabel("Possible IP Address:");
+        ipAddress.setBounds(50,20,250,25);
+        panel.add(ipAddress);
+
+        JScrollPane scrollPane = new JScrollPane(ipList);
+        scrollPane.setBounds(200,23,300,20);
+        panel.add(scrollPane);
 
         JFrame frame = new JFrame();
-        frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("PC to Android");
-        frame.pack();
+        frame.setSize(550,500);
+        frame.add(panel);
         frame.setVisible(true);
+        frame.setResizable(false);
 
     }
     /*
